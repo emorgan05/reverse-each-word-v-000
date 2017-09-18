d21d1a5a1c37556ded31c3385319333f3c70b362
Emily Morgan <emorgan05@gmail.com>
2017-09-18 20:51:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,10 @@ def reverse_each_word(string)
   end
   reversed_words.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |word|
+    word.reverse
+  end.join(" ")
+end
