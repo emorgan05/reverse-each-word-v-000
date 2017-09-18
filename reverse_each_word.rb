d4461d46a9b7aa7410d4cf5dcc11cb377e0fd527
Emily Morgan <emorgan05@gmail.com>
2017-09-18 20:54:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,5 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  array = string.split(" ")
-  array.collect do |word|
-    word.reverse
-  end.join(" ")
+  string.split(" ").collect {|word| word.reverse}.join(" ")
 end
